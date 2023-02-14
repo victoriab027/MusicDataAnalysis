b1c7e09b7544bb67d2a508aee4fa36ab44a8f796
victoriab027 <70912962+victoriab027@users.noreply.github.com>
2023-02-14 14:20:50 -0600

    working on React interface

---
+++ spotify-web-player/src/Webplayback.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+function WebPlayback(props) {
+
+   return (
+      <>
+        <div className="container">
+           <div className="main-wrapper">
+
+            </div>
+        </div>
+      </>
+    );
+}
+
+export default WebPlayback
